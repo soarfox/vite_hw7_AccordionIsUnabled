f8ef7e69705e71082971107fcf7d66eed876bad3
soarfox <soarfox@gmail.com>
2023-04-06 17:14:13 +0800

    edited some content

--- src/views/front/ServiceView.vue
+++ src/views/front/ServiceView.vue
@@ -9,7 +9,7 @@
         <h2 class="accordion-header" id="headingOne">
           <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
             aria-expanded="true" aria-controls="collapseOne">
-            Accordion Item #1
+            Accordion Item #117
           </button>
         </h2>
         <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
