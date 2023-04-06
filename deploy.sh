cf2ec8490106f0528931f1d960b83f241e6735a6
soarfox <soarfox@gmail.com>
2023-04-06 16:55:11 +0800

    edited comments of deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,8 @@ git commit -m 'deploy'
 # 部署到 https://soarfox.github.io/vite_hw7_AccordionIsUnabled
 # 已將下方master改成main, 替換成自己的帳號與repository網址, 記得要把預設的<>符號都拿掉
 # git push -f git@github.com:soarfox/vite_hw7_AccordionIsUnabled.git main:gh-pages
-# 上方那句無效, 故改用Wendy同學筆記裡的方式, 如下方那一行, 且儲存本檔案後, 請務必要找任意一個.vue檔案並更動其內容後儲存, 並且在VScode左側寫上commit註解及進行提交後, (在Windows作業系統下)請打開資料夾視窗, 請對本專案資料夾按下右鍵, 選擇以"git Bash here"方式打開bash視窗, 輸入這個指令sh deploy.sh後, 即可成功看到github上有gh-pages分支了, 請記得一定要做前述更動任意一個.vue檔案的動作, 這樣子使用指令sh deploy.sh後才會看到gh-pages分支(當Vue程式碼有任何更動後, 請記得先在VScode終端機內使用指令npm run build後, 再將變動的內容寫上註解並commit出去, 最後才使用(在Windows作業系統下)請打開資料夾視窗, 請對本專案資料夾按下右鍵, 選擇以"git Bash here"方式打開bash視窗, 輸入這個指令sh deploy.sh)
+# 上方那句無效, 故改用Wendy同學筆記裡的方式, 如下方那一行, 且儲存本檔案後, 請務必要找任意一個.vue檔案並更動其內容後儲存, 並且在VScode左側寫上commit註解及進行提交後, (在Windows作業系統下)請打開資料夾視窗, 請對本專案資料夾按下右鍵, 選擇以"git Bash here"方式打開bash視窗, 輸入這個指令sh deploy.sh後, 即可成功看到github上有gh-pages分支了, 請記得一定要做前述更動任意一個.vue檔案的動作, 這樣子使用指令sh deploy.sh後才會看到gh-pages分支
+# 當Vue程式碼有任何更動後, 請記得先在VScode終端機內使用指令npm run build後, 再將變動的內容寫上註解並commit出去, 最後才使用(在Windows作業系統下)請打開資料夾視窗, 請對本專案資料夾按下右鍵, 選擇以"git Bash here"方式打開bash視窗, 輸入這個指令sh deploy.sh
 git push -f https://github.com/soarfox/vite_hw7_AccordionIsUnabled.git main:gh-pages
 
 cd -
